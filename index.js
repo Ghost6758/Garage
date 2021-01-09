67c180e365f595253ccb1009cc0235dc40e9485f
Joe <47850783+JoeB6758@users.noreply.github.com>
2021-01-09 21:47:43 +0000

    Update

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require('file-system');
+require('dotenv').config();
+const { alert, backend, status, prefix} = require('./config.json')
 
-client.on('ready', ready => {
-    console.log('Cleaning up the garage...')
-})
-client.on('message', message => {
-    console.log('Functional.')
+client.on('ready', () => {
+    console.log('Cleaning up the garage...');
+});
+client.on('message', (message) => {
+    
+    if(message.author.bot || !message.content.startsWith(prefix) || message.channel.dm) return;
+
+    client.channels.cache.get(alert).send('Alert');
+    client.channels.cache.get(status).send('status');
+
 });
 
-client.login()
+client.login(process.env.token)
